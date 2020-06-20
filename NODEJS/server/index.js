502267eaeed795b4562dcf04ead058ec6dc2d08f
phuochoquang97 <56500909+phuochoquang97@users.noreply.github.com>
2020-06-20 14:27:34 +0700

    add ml module

--- NODEJS/server/index.js
+++ NODEJS/server/index.js
@@ -4,6 +4,7 @@ const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('db.json')
 const db = low(adapter)
+const data = require('./db.json');
 
 db.defaults({ data : [] })
   .write()
@@ -18,6 +19,74 @@ var bodyparser = require('body-parser');
 
 var server = require('http').Server(app);         //#Khởi tạo một chương trình mạng (app)
 var io = require('socket.io')(server);            //#Phải khởi tạo io sau khi tạo app!
+
+// Khai bao ML
+const tf = require('@tensorflow/tfjs');
+const { isObject } = require('util');
+
+tf.disableDeprecationWarnings();
+
+const trainingData = tf.tensor2d(data.map(item=> [
+    item.InsideTemp, item.InsideLux, item.OutsideTemp, item.OutsideLux
+]
+),[653,4])
+
+const outputData = tf.tensor2d(data.map(item => [
+    item.Servo == 0 ? 1 : 0,
+    item.Servo == 30 ? 1 : 0,
+    item.Servo == 45 ? 1 : 0,
+    item.Servo == 60 ? 1 : 0,
+    item.Servo == 90 ? 1 : 0,
+    item.Servo == 120 ? 1 : 0,
+    item.Servo == 135 ? 1 : 0,
+    item.Servo == 150 ? 1 : 0,
+    item.Servo == 180 ? 1 : 0,
+
+]), [653,9])
+
+const model = tf.sequential();
+
+
+model.add(tf.layers.dense(
+    {   inputShape: 4, 
+        activation: 'sigmoid', 
+        units: 50
+    }
+));
+
+model.add(tf.layers.dense(
+    {
+        inputShape: 50,
+        activation: 'sigmoid',
+        units: 10
+    }
+))
+
+model.add(tf.layers.dense(
+    {
+        inputShape: 10, 
+        units: 9, 
+        activation: 'softmax'
+    }
+));
+
+model.summary();
+
+model.compile({
+    loss: "categoricalCrossentropy",
+    optimizer: tf.train.adam()
+});
+
+async function train_data(){
+
+	console.log("Training Started");
+    for(let i=0;i<10;i++){
+		let res = await model.fit(trainingData, outputData, {epochs: 50});
+		console.log(`Iteration ${i}: ${res.history.loss[0]}`);
+	}
+	console.log("Training Complete");
+}
+//Ket thuc khai bao ML
 
 server.listen(PORT, function () {
     console.log("Server Nodejs chay tai dia chi: " + ip.address() + ":" + PORT)
@@ -55,6 +124,57 @@ function getDataOutside(vtemp, vlux, vposServo, vtime) {
                 Servo : vposServo,
             })
       .write();
+    //START ML
+
+        var test = tf.tensor2d([parseFloat(inside.temp), parseFloat(inside.lux), parseFloat(vtemp), parseFloat(vlux)], [1,4]);
+        console.log('test: '+test);
+        
+        var out = model.predict(test);
+        console.log('out'+out);
+    
+        var maxIndex = 0;
+        for (let i=1;i<out.size; i++){
+            if (out.buffer().get(0, i) > out.buffer().get(0, maxIndex)){
+                maxIndex = i;
+            }
+        }
+    
+        console.log('max index = ' + maxIndex);
+    
+        ans = "Undetermined";
+    
+        switch(maxIndex) {
+            case 0:
+                ans = '0';	
+            break;
+            case 1:
+                ans = '30';	
+            break;
+            case 2:
+                ans = '45';	
+            break;	
+            case 3:
+                ans = '60';	
+            break;
+            case 4:
+                ans = '90';	
+            break;
+            case 5:
+                ans = '120';	
+            break;
+            case 6:
+                ans = '135';	
+            break;
+            case 7:
+                ans = '150';	
+            break;
+            case 8:
+                ans = '180';	
+            break;
+        }
+
+        io.sockets.emit('setServo', parseInt(ans));
+    //END ML
       
     io.sockets.emit('sendDataOutside', {
                                             TimeStamp: vtime,
@@ -72,6 +192,7 @@ io.on('connection', function (socket) {
 
     socket.on('Connect', function (message) {
         console.log(message);
+        train_data();
     });
 
     socket.on('requestData', function(check){
